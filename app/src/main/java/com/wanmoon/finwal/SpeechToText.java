6b83f5389a4509d63f87545f0893c70f0326d774
poysrnp <poysrnp@gmail.com>
2017-08-06 17:00:12 +0700

    debug move code from mainactivity to speechtotext // mainactivity is signup

--- app/src/main/java/com/wanmoon/finwal/SpeechToText.java
+++ app/src/main/java/com/wanmoon/finwal/SpeechToText.java
@@ -35,7 +35,7 @@ public class SpeechToText extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.speechtotext);
         resultTEXT = (TextView)findViewById(R.id.TvResult);
         imageButton = (ImageButton)findViewById(R.id.imageButton);
 
