1728a914c4a370d7465c8173d6f530b8611ebdf2
show <varn_T_T@hotmail.com>
2017-08-09 17:09:19 +0700

    start ForgetPasword step

--- app/src/main/java/com/wanmoon/finwal/ForgotPassword.java
+++ app/src/main/java/com/wanmoon/finwal/ForgotPassword.java
@@ -1,8 +1,29 @@
 package com.wanmoon.finwal;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 /**
  * Created by pimpischaya on 8/6/2017 AD.
  */
 
-public class ForgotPassword {
+public class ForgotPassword extends AppCompatActivity {
+    private EditText etMail;
+    private Button btnResetPsw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgetpassword);
+        etMail = (EditText)findViewById(R.id.editTextEmail);
+        btnResetPsw = (Button)findViewById(R.id.buttonResetPSW);
+    }
+
+    public void Button(View view){
+
+    }
+
 }
