c68aa5288c7dcb56c14176383a1cc9ab74c56ccf
poysrnp <poysrnp@gmail.com>
2017-08-23 17:05:21 +0700

    speechtotext no category

--- app/src/main/java/com/wanmoon/finwal/activity/SpeechToText.java
+++ app/src/main/java/com/wanmoon/finwal/activity/SpeechToText.java
@@ -1,5 +1,6 @@
 package com.wanmoon.finwal.activity;
 
+import android.app.Dialog;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,6 +8,8 @@ import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -38,6 +41,39 @@ public class SpeechToText extends AppCompatActivity implements View.OnClickListe
     private TextView textViewFinish;
     private TextView textViewCancel;
 
+    private Dialog incomeCate;
+    private Dialog expenseCate;
+
+
+    private Button buttonPlus;
+    private Button buttonMinus;
+
+
+    private Button buttonSalary;
+    private Button buttonLoan;
+    private Button buttonGift;
+    private Button buttonFamilyIncome;
+    private Button buttonExtra;
+
+    //button expense
+    private Button buttonBill;
+    private Button buttonEducation;
+    private Button buttonEntertainment;
+    private Button buttonFood;
+    private Button buttonShopping;
+    private Button buttonTransport;
+    private Button buttonTravel;
+    private Button buttonFamilyExpense;
+    private Button buttonHealthCare;
+    private Button buttonSaving;
+
+    private String transaction;
+    private String cate;
+
+    private TextView textViewCategories;
+    private TextView textViewTransaction;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +88,9 @@ public class SpeechToText extends AppCompatActivity implements View.OnClickListe
         textViewFinish.setOnClickListener(this);
         textViewCancel.setOnClickListener(this);
 
+
+        buttonPlus = (Button) findViewById(R.id.buttonPlus);
+        buttonMinus = (Button) findViewById(R.id.buttonMinus);
 
     }
 
@@ -145,6 +184,8 @@ public class SpeechToText extends AppCompatActivity implements View.OnClickListe
                         );
                     }
                 }
+                button();
+
 
             }
 
@@ -152,6 +193,236 @@ public class SpeechToText extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+    public void button(){
+
+        textViewTransaction = (TextView) findViewById(R.id.textViewTransaction);
+        textViewCategories = (TextView) findViewById(R.id.textViewCategories);
+
+        buttonPlus.setVisibility(View.VISIBLE);
+        buttonPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                incomeCate = new Dialog(SpeechToText.this);
+                incomeCate.getWindow();
+                incomeCate.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                incomeCate.setContentView(R.layout.income_cate);
+                incomeCate.setCancelable(true);
+                incomeCate.show();
+
+                transaction = "Income";
+                textViewTransaction.setText("Transaction : " + transaction);
+
+
+                // income button
+                buttonGift = (Button) incomeCate.findViewById(R.id.buttonGift);
+                buttonGift.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        cate = buttonGift.getText().toString();
+                        Toast.makeText(SpeechToText.this, cate + " Income", Toast.LENGTH_LONG).show();
+                        incomeCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+
+                buttonSalary = (Button) incomeCate.findViewById(R.id.buttonSalary);
+                buttonSalary.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        cate = buttonSalary.getText().toString();
+                        Toast.makeText(SpeechToText.this, cate + " Income", Toast.LENGTH_LONG).show();
+                        incomeCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+
+                buttonLoan = (Button) incomeCate.findViewById(R.id.buttonLoan);
+                buttonLoan.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        cate = buttonLoan.getText().toString();
+                        Toast.makeText(SpeechToText.this, cate + " Income", Toast.LENGTH_LONG).show();
+                        incomeCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+
+                buttonFamilyIncome = (Button) incomeCate.findViewById(R.id.buttonFamilyIncome);
+                buttonFamilyIncome.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        cate = buttonFamilyIncome.getText().toString();
+                        Toast.makeText(SpeechToText.this, cate + " Income", Toast.LENGTH_LONG).show();
+                        incomeCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+
+                buttonExtra = (Button) incomeCate.findViewById(R.id.buttonExtra);
+                buttonExtra.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        cate = buttonExtra.getText().toString();
+                        Toast.makeText(SpeechToText.this, cate, Toast.LENGTH_LONG).show();
+                        incomeCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+
+            }
+        });
+
+        buttonMinus.setVisibility(View.VISIBLE);
+        buttonMinus = (Button) findViewById(R.id.buttonMinus);
+        buttonMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                expenseCate = new Dialog(SpeechToText.this);
+                expenseCate.getWindow();
+                expenseCate.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                expenseCate.setContentView(R.layout.expense_cate);
+                expenseCate.setCancelable(true);
+                expenseCate.show();
+
+                transaction = "Expense";
+                textViewTransaction.setText("Transaction : " + transaction);
+
+                //expense's button
+                buttonBill = (Button) expenseCate.findViewById(R.id.buttonBill);
+                buttonBill.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        cate = buttonBill.getText().toString();
+                        Toast.makeText(SpeechToText.this,cate + " Expense", Toast.LENGTH_LONG).show();
+                        expenseCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+
+                buttonEducation = (Button) expenseCate.findViewById(R.id.buttonEducation);
+                buttonEducation.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v){
+                        cate = buttonEducation.getText().toString();
+                        Toast.makeText(SpeechToText.this,cate + " Expense", Toast.LENGTH_LONG).show();
+                        expenseCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+
+                buttonEntertainment = (Button) expenseCate.findViewById(R.id.buttonEntertainment);
+                buttonEntertainment.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        cate = buttonEntertainment.getText().toString();
+                        Toast.makeText(SpeechToText.this,cate + " Expense", Toast.LENGTH_LONG).show();
+                        expenseCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+
+                buttonFood = (Button) expenseCate.findViewById(R.id.buttonFood);
+                buttonFood.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        cate = buttonFood.getText().toString();
+                        Toast.makeText(SpeechToText.this,cate + " Expense", Toast.LENGTH_LONG).show();
+                        expenseCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+
+                buttonShopping = (Button) expenseCate.findViewById(R.id.buttonShopping);
+                buttonShopping.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        cate = buttonShopping.getText().toString();
+                        Toast.makeText(SpeechToText.this,cate + " Expense", Toast.LENGTH_LONG).show();
+                        expenseCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+
+                buttonTransport = (Button) expenseCate.findViewById(R.id.buttonTransport);
+                buttonTransport.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        cate = buttonTransport.getText().toString();
+                        Toast.makeText(SpeechToText.this,cate + " Expense", Toast.LENGTH_LONG).show();
+                        expenseCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+
+                buttonTravel = (Button) expenseCate.findViewById(R.id.buttonTravel);
+                buttonTravel.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        cate = buttonTravel.getText().toString();
+                        Toast.makeText(SpeechToText.this,cate + " Expense", Toast.LENGTH_LONG).show();
+                        expenseCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+
+                buttonFamilyExpense = (Button) expenseCate.findViewById(R.id.buttonFamilyExpense);
+                buttonFamilyExpense.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        cate = buttonFamilyExpense.getText().toString();
+                        Toast.makeText(SpeechToText.this,cate, Toast.LENGTH_LONG).show();
+                        expenseCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+
+                buttonHealthCare = (Button) expenseCate.findViewById(R.id.buttonHealthCare);
+                buttonHealthCare.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        cate = buttonHealthCare.getText().toString();
+                        Toast.makeText(SpeechToText.this,cate + " Expense", Toast.LENGTH_LONG).show();
+                        expenseCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+
+                buttonSaving = (Button) expenseCate.findViewById(R.id.buttonSaving);
+                buttonSaving.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        cate = buttonSaving.getText().toString();
+                        Toast.makeText(SpeechToText.this,cate + " Expense", Toast.LENGTH_LONG).show();
+                        expenseCate.cancel();
+                        textViewCategories.setText("Categories : " + cate);
+                    }
+                });
+            }
+        });
+
+
+    }
+
 
     @Override
     public void onClick(View v) {
